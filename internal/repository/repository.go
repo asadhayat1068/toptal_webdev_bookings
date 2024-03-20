c8c0b2c0ca60948925dba693d47d25f4466ace25
Asad Hayat <asadhayat.1068@gmail.com>
2024-03-21 01:23:15 +0500

    Added database action for SearchAvailabilityByDatesByRoomID

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -1,9 +1,14 @@
 package repository
 
-import "github.com/asadhayat1068/toptal_webdev_bookings/internal/models"
+import (
+	"time"
+
+	"github.com/asadhayat1068/toptal_webdev_bookings/internal/models"
+)
 
 type DatabaseRepo interface {
 	AllUsers() bool
 	InsertReservation(res models.Reservation) (int, error)
 	InsertRoomRestriction(r models.RoomRestriction) (int, error)
+	SearchAvailabilityByDatesByRoomID(roomID int, start, end time.Time) (bool, error)
 }
