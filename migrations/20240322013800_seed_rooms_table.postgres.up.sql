83da13def2bf5538aa68a04c17080961a081a63e
Asad Hayat <asadhayat.1068@gmail.com>
2024-03-22 06:40:34 +0500

    Added migration to seed rooms table

---
+++ migrations/20240322013800_seed_rooms_table.postgres.up.sql
@@ -0,0 +1,7 @@
+INSERT INTO public.rooms (room_name,created_at,updated_at)
+VALUES ('General''s Quarters',
+        '2024-03-20 18:00:00.000',
+        '2024-03-20 18:00:00.000'), ('Major''s Suit',
+                                     '2024-03-20 18:00:00.000',
+                                     '2024-03-20 18:00:00.000');
+
