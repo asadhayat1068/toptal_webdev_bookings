480f0a71d0f650d9ab725608b3424694af377bdd
Asad Hayat <asadhayat.1068@gmail.com>
2024-03-24 00:53:12 +0500

    Adding SQL migration to seed restrictions table

---
+++ migrations/20240322014054_seed_restrictions_table.postgres.up.sql
@@ -0,0 +1,7 @@
+INSERT INTO restrictions (restriction_name,created_at,updated_at)
+VALUES ('Reservation',
+        '2024-03-20 18:00:00.000',
+        '2024-03-20 18:00:00.000'), ('Owner Block',
+                                     '2024-03-20 00:00:00.000',
+                                     '2024-03-20 00:00:00.000');
+
